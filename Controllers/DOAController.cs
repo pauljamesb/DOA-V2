99c8e6a8d9739f51e60fd56180a8f022e7cd6eae
pauljamesb <37079324+pauljamesb@users.noreply.github.com>
2018-09-27 14:17:48 +0100

    Create DOAController.cs

---
+++ Controllers/DOAController.cs
@@ -0,0 +1,16 @@
+// Usings go here
+
+using DOA.Models;
+
+namespace DOA.Controllers
+{
+      public class DOAController : Controllers
+      {
+
+          public IActionResult Index()
+          {
+                return View();
+          }
+
+      }
+}
